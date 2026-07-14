a0d3e28a8b0f8630f1c3797677714ecc0cd17a96
Klesti Selimaj <klesti.selimay@icloud.com>
2026-07-14 12:15:50 -0400

    App structure

--- pulse-ui/src/lib.rs
+++ pulse-ui/src/lib.rs
@@ -2,3 +2,24 @@ pub mod layout;
 pub mod render;
 pub mod unit;
 pub mod widget;
+
+pub trait App {
+    // Rendering
+    fn layout(&self) -> layout::Allocation;
+    fn render(&mut self, layout: layout::Allocation);
+
+    // State
+    fn init(&mut self);
+
+    fn update(&mut self);
+
+    fn refresh(&mut self) {
+        self.render(self.layout());
+    }
+
+    // Run
+    fn run(&mut self) {
+        self.init();
+        self.refresh();
+    }
+}
