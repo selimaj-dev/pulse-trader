712a149735f5cc85ea996a3f43711db7d98251a6
Klesti Selimaj <klesti.selimay@icloud.com>
2026-07-14 12:18:28 -0400

    Update app structure

--- pulse-ui/src/lib.rs
+++ pulse-ui/src/lib.rs
@@ -4,22 +4,28 @@ pub mod unit;
 pub mod widget;
 
 pub trait App {
+    // State
+    fn init(&mut self);
+    fn update(&mut self);
+    fn tick(&self);
+
     // Rendering
     fn layout(&self) -> layout::Allocation;
     fn render(&mut self, layout: layout::Allocation);
-
-    // State
-    fn init(&mut self);
-
-    fn update(&mut self);
 
     fn refresh(&mut self) {
         self.render(self.layout());
     }
 
-    // Run
+    // Running
     fn run(&mut self) {
         self.init();
         self.refresh();
+
+        loop {
+            self.tick();
+            self.update();
+            self.refresh();
+        }
     }
 }
