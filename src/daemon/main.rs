ce1ebc74b12d0271410208410d55cba857d388e1
Klesti Selimaj <klesti.selimay@icloud.com>
2026-07-22 14:34:16 +0200

    Working protocol

--- src/daemon/main.rs
+++ src/daemon/main.rs
@@ -1,5 +1,22 @@
 pub mod ptc;
 
+use std::time::Instant;
+
+use ptc::PulseCom;
+
 fn main() {
-    println!("Hello, World!")
+    let input = ptc::EventLog {
+        kind: ptc::LogKind::Warn,
+        name: "Test".to_string(),
+        message: "Hello, WOrld".to_string(),
+    };
+
+    let start = Instant::now();
+
+    let mut val = input.to_com();
+    let out = ptc::EventLog::from_com(&mut val);
+
+    let elapsed = start.elapsed();
+
+    println!("{:?} {:?}", out, elapsed);
 }
